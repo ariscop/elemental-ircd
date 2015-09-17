36f015d8acfc289be77693822be3290f4011c4a8
Andrew Cook <ariscop@gmail.com>
2015-09-17 14:52:08 +1000

    Misc changes

--- scripts/travis_genssl.sh
+++ scripts/travis_genssl.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+cd `dirname $0`
+testdir=`pwd`
+prefix=`sed -n -e 's/^#define IRCD_PREFIX "\(.*\)"/\1/p' "$testdir/../include/setup.h"`
+[ -d $prefix ] || { echo Unable to find installation prefix; exit 1; }
 
 echo "Using the pregenned cert"
 
-cp testsuite/ssl* ~/ircd/etc
-cp testsuite/dh.pem ~/ircd/etc
+cp testsuite/ssl* $prefix/etc
+cp testsuite/dh.pem $prefix/etc
